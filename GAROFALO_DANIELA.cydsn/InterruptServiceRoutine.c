3ff309ecef72d3adfe2fa8fa1353cd26f831dd38
Daniela Garofalo <daniela.garofalo@mail.polimi.it>
2020-10-12 20:58:25 +0200

    Implementation of the InterruptServiceRoutine files

--- GAROFALO_DANIELA.cydsn/InterruptServiceRoutine.c
+++ GAROFALO_DANIELA.cydsn/InterruptServiceRoutine.c
@@ -10,4 +10,27 @@
  * ========================================
 */
 
+/*
+    File InterruptServiceRoutine.c
+    Function aimed to update the state of the device as a function of the output of the hardware debouncer connected to the switch
+    Author: Daniela Garofalo
+*/
+
+# include <InterruptServiceRoutine.h>
+
+# define BUTTON_PRESSED 0
+int stato;
+
+CY_ISR(custom_PushButton_ISR)
+{
+    if (PushButton_Read() == BUTTON_PRESSED)
+    {
+        stato++;
+        if (stato > 7)
+        {
+            stato = 1;
+        }
+    }
+}
+
 /* [] END OF FILE */
--- GAROFALO_DANIELA.cydsn/InterruptServiceRoutine.h
+++ GAROFALO_DANIELA.cydsn/InterruptServiceRoutine.h
@@ -10,4 +10,19 @@
  * ========================================
 */
 
+/*
+    File InterruptServiceRoutine.h
+    Function aimed to include the libraries needed to implement the ISR related to the hardware debouncer
+    Author: Daniela Garofalo
+*/
+
+#ifndef _INTERRUPT_SERVICE_ROUTINE_H_ // Verifica se la dichiarazione è già stata eseguita una volta 
+    #define _INTERRUPT_SERVICE_ROUTINE_H_
+    
+    #include <PushButton.h>
+    
+    CY_ISR_PROTO(custom_PushButton_ISR);
+    
+#endif
+
 /* [] END OF FILE */
